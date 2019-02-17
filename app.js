6a09e689577500b8bf2962affe14bc7e22bbaaca
Aleš Pros <ales.pros@epptec.local>
2019-02-17 15:46:27 +0100

    Upload file dialog added to the room.htm

--- app.js
+++ app.js
@@ -20,8 +20,6 @@ chatApp.controller("homeController", ["$scope", "$location", "$cookies", "fireba
         $cookies.put("email", $scope.email);
         
         firebaseDatabase.createChatRoom();
-        
-        
     }
     
 }]);
@@ -64,6 +62,12 @@ chatApp.controller("roomController", ["$scope", "$cookies", "$timeout", "$fireba
         } else {
             document.execCommand(command, false, null);
         }
+    }
+    
+    $scope.uploadFile = function() {
+        var selectedFile = $("#upload-file")[0].files[0]
+        var folderInFirebaseStorage = "/files/"
+        firebaseDatabase.uploadFile(selectedFile, folderInFirebaseStorage);
     }
 }]);
 
@@ -148,6 +152,44 @@ chatApp.service("firebaseDatabase", ["$firebaseArray", "$timeout", "$location", 
                 console.log("Error:", error);
             });
     }
+    
+    this.uploadFile = function(selectedFile, folderInFirebaseStorage) {        
+        if(selectedFile === undefined) {
+            console.log("Select the file, please.")
+        } else {
+            var filename = selectedFile.name;
+            var storageRef = firebase.storage().ref(folderInFirebaseStorage + filename);
+
+            var uploadTask = storageRef.put(selectedFile);
+
+            // Register three observers:
+            // 1. 'state_changed' observer, called any time the state changes
+            // 2. Error observer, called on failure
+            // 3. Completion observer, called on successful completion
+            uploadTask.on('state_changed', function(snapshot){
+              // Observe state change events such as progress, pause, and resume
+              // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+              var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              console.log('Upload is ' + progress + '% done');
+              switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED: // or 'paused'
+                  console.log('Upload is paused');
+                  break;
+                case firebase.storage.TaskState.RUNNING: // or 'running'
+                  console.log('Upload is running');
+                  break;
+              }
+            }, function(error) {
+              // Handle unsuccessful uploads
+            }, function() {
+              // Handle successful uploads on complete
+              // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+              uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                console.log('File available at', downloadURL);
+              });
+            });
+        }
+    };
 }])
 
 chatApp.service("modalService", [function() {
